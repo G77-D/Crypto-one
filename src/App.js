b5dc3b13a1d04498b7ca4b581d0ce57c23f97afc
Gary Dineen <dineen77@hotmail.com>
2021-04-05 09:48:28 +0200

    updates

--- src/App.js
+++ src/App.js
@@ -1,13 +1,32 @@
-
-import './App.css';
+import axios from "axios";
+import React, { useState, useEffect } from "react";
+import "./App.css";
 
 function App() {
-  return (
-    <div>
+	const [coins, setCoins] = useState([]);
 
-<h1>hello</h1>
-    </div>
-  );
+	const [search, setSearch] = useState("");
+
+	useEffect(() => {
+		axios
+			.get("https://api.coingecko.com/api/v3/coins/markets?vs_currency=Eur&order=market_cap_desc&page=100&sparkline=false&locale=en"
+			)
+			.then((res) => {
+				setCoins(res.data.coins);
+			})
+			.catch((error) => console.log(error));
+	}, []);
+
+	return (
+		<div className="coin-app">
+			<div className="coin-search">
+				<h1 className="coin-text"> Search a currency </h1>
+				<form>
+					<input type="text" placeholder="Search" className="coin-input" />
+				</form>
+			</div>
+		</div>
+	);
 }
 
 export default App;
