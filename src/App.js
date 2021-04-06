2f08c694801de5119afbc0e760e6a638ff80712b
Gary Dineen <dineen77@hotmail.com>
2021-04-06 21:40:01 +0200

    adding code

--- src/App.js
+++ src/App.js
@@ -1,32 +1,54 @@
-import axios from "axios";
-import React, { useState, useEffect } from "react";
-import "./App.css";
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+//import './App.css';
+import Coin from './Coin'
 
-function App() {
-	const [coins, setCoins] = useState([]);
 
-	const [search, setSearch] = useState("");
+function App(){
+	const [coins, setCoins] = useState ([]); 
+	const [search, setSearch] = useState ('')
+
 
 	useEffect(() => {
-		axios
-			.get("https://api.coingecko.com/api/v3/coins/markets?vs_currency=Eur&order=market_cap_desc&page=100&sparkline=false&locale=en"
-			)
+		axios.get ("https://api.coingecko.com/api/v3/coins/markets?vs_currency=eur&order=market_cap_desc&per_page=10&page=1&sparkline=false&locale=en")
 			.then((res) => {
-				setCoins(res.data.coins);
-			})
-			.catch((error) => console.log(error));
+				setCoins(res.data);
+			}).catch(error =>console.log(error));
 	}, []);
 
+	const handleChange = e => {
+		setSearch(e.target.value);
+	}
+
+
+const filteredCoins = coins.filter(coin =>
+	coin.name.toLowerCase().includes(search.toLowerCase())
+	)
+
 	return (
-		<div className="coin-app">
-			<div className="coin-search">
-				<h1 className="coin-text"> Search a currency </h1>
-				<form>
-					<input type="text" placeholder="Search" className="coin-input" />
-				</form>
-			</div>
-		</div>
+	<div className='coin-ap'>
+	<div className='coin-search'>
+		<h1 className='coin-text'>Search a Currency</h1>
+		<form>
+			<input type='text' placeholder='Search' className='coin-input' onChange= {handleChange} />
+		</form>
+	</div>
+	{filteredCoins.map(coin=>{
+		return (
+			<Coin
+				key={coin.id}
+				name={coin.name}
+				image={coin.image}
+				symbol={coin.symbol}
+				marketcap={coin.market_cap}
+				volume={coin.total_volume}
+				price={coin.price}
+				priceChange={coin.price_change_percentage_24h}
+			/>
+		);
+	})}
+	</div>
 	);
 }
 
-export default App;
+export default App
